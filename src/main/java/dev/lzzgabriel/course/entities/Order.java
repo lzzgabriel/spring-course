0b41f61c6772afa4fa4d6251e73c0fa2ca39638a
lzzgabriel <96301579+lzzgabriel@users.noreply.github.com>
2024-01-31 08:19:34 -0300

    Order, Instant, ISO 8601 - Part 1

---
+++ src/main/java/dev/lzzgabriel/course/entities/Order.java
@@ -0,0 +1,80 @@
+package dev.lzzgabriel.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+  private static final long serialVersionUID = 1L;
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private Instant momento;
+
+  @ManyToOne
+  @JoinColumn(name = "client_id")
+  private User client;
+
+  public Order() {
+  }
+
+  public Order(Long id, Instant momento, User client) {
+    super();
+    this.id = id;
+    this.momento = momento;
+    this.client = client;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Instant getMomento() {
+    return momento;
+  }
+
+  public void setMomento(Instant momento) {
+    this.momento = momento;
+  }
+
+  public User getClient() {
+    return client;
+  }
+
+  public void setClient(User client) {
+    this.client = client;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Order other = (Order) obj;
+    return Objects.equals(id, other.id);
+  }
+
+}
