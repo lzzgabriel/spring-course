0c3f270708ed91a9da7ae146d63a7a1492db4e42
lzzgabriel <96301579+lzzgabriel@users.noreply.github.com>
2024-02-02 08:55:08 -0300

    Payment, one-to-one association

--- src/main/java/dev/lzzgabriel/course/config/TestConfig.java
+++ src/main/java/dev/lzzgabriel/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import dev.lzzgabriel.course.entities.Category;
 import dev.lzzgabriel.course.entities.Order;
 import dev.lzzgabriel.course.entities.OrderItem;
+import dev.lzzgabriel.course.entities.Payment;
 import dev.lzzgabriel.course.entities.Product;
 import dev.lzzgabriel.course.entities.User;
 import dev.lzzgabriel.course.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
     OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
     
     orderItemRepository.saveAll(List.of(oi1,oi2,oi3,oi4));
+    
+    Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+    o1.setPayment(pay1);
+    
+    orderRepository.save(o1);
     
   }
 
